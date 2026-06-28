7e6951d9af2397f32cdb4312845e0a9bfc941db0
William <tsygod777@gmail.com>
2026-06-28 19:11:19 +0800

    implement home page using Trading View widgets

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -21,7 +21,7 @@ import NavItems from "@/components/NavItems";
 const UserDropdown = () => {
     const router = useRouter()
 
-    const handleSignout = async () => {
+    const handleSignOut = async () => {
         router.push("/sign-in");
     }
 
@@ -45,25 +45,24 @@ const UserDropdown = () => {
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent className="w-50 text-gray-400">
-               <DropdownMenuLabel>
-                   <div className="flex relative items-center gap-3 py-2">
-                       <Avatar className="h-10 w-10">
-                           <AvatarImage src="https://github.com/shadcn.png"/>
-                           <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
-                               {user.name[0]}
-                           </AvatarFallback>
-                       </Avatar>
-                       <div className="flex flex-col">
-                       <span className="text-base font-medium text-gray-400">
-                            {user.name}
-                       </span>
-                           <span className="text-sm text-gray-500">{user.email}</span>
-                       </div>
-
-                   </div>
-               </DropdownMenuLabel>
+                <DropdownMenuLabel>
+                    <div className="flex relative items-center gap-3 py-2">
+                        <Avatar className="h-10 w-10">
+                            <AvatarImage src="https://github.com/shadcn.png"/>
+                            <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
+                                {user.name[0]}
+                            </AvatarFallback>
+                        </Avatar>
+                        <div className="flex flex-col">
+                            <span className="text-base font-medium text-gray-400">
+                                 {user.name}
+                            </span>
+                            <span className="text-sm text-gray-500">{user.email}</span>
+                        </div>
+                    </div>
+                </DropdownMenuLabel>
                 <DropdownMenuSeparator className="bg-gray-600"/>
-                <DropdownMenuItem onClick={handleSignout} className="text-gray-100 text-md font-medium focus:bg-transparent focus:text-yellow-500 transition-colors cursor-pointer">
+                <DropdownMenuItem onClick={handleSignOut} className="text-gray-100 text-md font-medium focus:bg-transparent focus:text-yellow-500 transition-colors cursor-pointer">
                     <LogOut className="h-4 w-4 mr-2 hidden sm:block" />
                     Logout
                 </DropdownMenuItem>
